21bf7caa9cdd6735e76926a5d7762261b3a94a53
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-07-15 21:50:47 -0500

    change uppercases

--- models/burger.js
+++ models/burger.js
@@ -17,7 +17,7 @@ Burger.selectBurgers = function () {
 
 Burger.create = function (burger) {
     return new Promise((resolve, reject) => {
-        orm.insertOne("BURGERS", {
+        orm.insertOne("burgers", {
             burger_name: burger.name,
             devoured: burger.devoured
         }).then(results => {
@@ -31,7 +31,7 @@ Burger.create = function (burger) {
 
 Burger.updateDevoured = function (burgerId) {
     return new Promise((resolve, reject) => {
-        orm.updateOne("BURGERS", "DEVOURED", true, "ID", burgerId).then(results => {
+        orm.updateOne("burgers", "devoured", true, "id", burgerId).then(results => {
             resolve(results);
         }).catch(() => {
             reject("Could not update burger");
